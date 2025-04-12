8f19ddd01e2510b3efb0588389aa918596d4bf63
minwoo <59003468+KoreanTuna@users.noreply.github.com>
2025-04-12 23:19:31 +0900

    [Silver IV] Title: 카드, Time: 232 ms, Memory: 34280 KB -BaekjoonHub

---
+++ node.js/백준/Silver/11652. 카드/카드.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const { log } = require("console");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let dataCount = parseInt(input[0]);
+let hash = new Map();
+
+var input_arr = input.slice(1, 1 + dataCount);
+input_arr.forEach((v, i) => {
+  hash.set(v, (hash.get(v) || 0) + 1);
+});
+
+var hash2 = new Map();
+
+hash.forEach((v, i) => {
+  if (hash2.has(v)) {
+    if (hash2.get(v) > BigInt(i)) {
+      // 제가 조사하다보니까 js에서 큰 숫자는 따로 BigInt를 이용해야한다네요!
+      hash2.set(v, BigInt(i));
+    }
+  } else {
+    hash2.set(v, BigInt(i));
+  }
+});
+
+var ans_arr = [];
+hash2.forEach((v, i) => {
+  ans_arr.push([i, v]);
+});
+
+ans_arr = ans_arr.sort((a, b) => b[0] - a[0]);
+
+console.log(ans_arr[0][1].toString());
