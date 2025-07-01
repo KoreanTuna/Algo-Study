01d13bec540157179fd7dfc4b10ce669329094c6
minwoo <59003468+KoreanTuna@users.noreply.github.com>
2025-07-01 13:48:06 +0900

    [level 2] Title: 더 맵게, Time: 1553.12 ms, Memory: 129 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int s : scoville) {
+            pq.offer(s);
+        }
+
+        while (pq.size() > 1 && pq.peek() < K) {
+            int first = pq.poll();
+            int second = pq.poll();
+            int newScoville = first + (second * 2);
+            pq.offer(newScoville);
+            answer++;
+        }
+
+        return pq.peek() >= K ? answer : -1;
+    }
+}
